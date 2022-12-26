57b67ef8255c049dedc8f70572751f3f36008b11
Yusuf Ismail <yusuf@qfsw.co.uk>
2022-12-26 19:56:34 +0000

    Better cube

--- src/demo/demo_main.cpp
+++ src/demo/demo_main.cpp
@@ -10,30 +10,56 @@ namespace demo
 {
     std::vector<Vector3f> vertices =
     {
-        Vector3f(-1, -1, -1) * 0.5f,
-	    Vector3f(1, -1, -1) * 0.5f,
-	    Vector3f(1, 1, -1) * 0.5f,
-	    Vector3f(-1, 1, -1) * 0.5f,
-	    Vector3f(-1, -1, 1) * 0.5f,
-	    Vector3f(1, -1, 1) * 0.5f,
-	    Vector3f(1, 1, 1) * 0.5f,
-	    Vector3f(-1, 1, 1) * 0.5f
+        Vector3f(-0.5f, -0.5f, -0.5f),
+	    Vector3f(0.5f, -0.5f, -0.5f),
+	    Vector3f(0.5f, 0.5f, -0.5f),
+	    Vector3f(-0.5f, 0.5f, -0.5f),
+
+        Vector3f(-0.5f, -0.5f, 0.5f),
+        Vector3f(0.5f, -0.5f, 0.5f),
+        Vector3f(0.5f, 0.5f, 0.5f),
+        Vector3f(-0.5f, 0.5f, 0.5f),
+
+        Vector3f(-0.5f, -0.5f, -0.5f),
+        Vector3f(-0.5f, 0.5f, -0.5f),
+        Vector3f(-0.5f, 0.5f, 0.5f),
+        Vector3f(-0.5f, -0.5f, 0.5f),
+
+        Vector3f(0.5f, -0.5f, -0.5f),
+        Vector3f(0.5f, 0.5f, -0.5f),
+        Vector3f(0.5f, 0.5f, 0.5f),
+        Vector3f(0.5f, -0.5f, 0.5f),
+
+        Vector3f(-0.5f, 0.5f, -0.5f),
+        Vector3f(0.5f, 0.5f, -0.5f),
+        Vector3f(0.5f, 0.5f, 0.5f),
+        Vector3f(-0.5f, 0.5f, 0.5f),
+
+        Vector3f(-0.5f, -0.5f, -0.5f),
+        Vector3f(0.5f, -0.5f, -0.5f),
+        Vector3f(0.5f, -0.5f, 0.5f),
+        Vector3f(-0.5f, -0.5f, 0.5f),
     };
 
     std::vector<Vector3u> indices =
     {
         Vector3u(0, 1, 3),
     	Vector3u(3, 1, 2),
-	    Vector3u(1, 5, 2),
-    	Vector3u(2, 5, 6),
-	    Vector3u(5, 4, 6),
-    	Vector3u(6, 4, 7),
-	    Vector3u(4, 0, 7),
-    	Vector3u(7, 0, 3),
-	    Vector3u(3, 2, 7),
-    	Vector3u(7, 2, 6),
-	    Vector3u(4, 5, 0),
-    	Vector3u(0, 5, 1)
+
+        Vector3u(4, 5, 7),
+        Vector3u(7, 5, 6),
+
+        Vector3u(8, 9, 11),
+        Vector3u(11, 9, 10),
+
+        Vector3u(12, 13, 15),
+        Vector3u(15, 13, 14),
+
+        Vector3u(16, 17, 19),
+        Vector3u(19, 17, 18),
+
+        Vector3u(20, 21, 23),
+        Vector3u(23, 21, 22),
     };
 
     std::vector<Vector3f> colors =
@@ -46,6 +72,33 @@ namespace demo
         Vector3f(1, 0, 1),
         Vector3f(1, 1, 1),
         Vector3f(0, 0, 0),
+
+        Vector3f(1, 1, 1),
+        Vector3f(1, 0, 1),
+        Vector3f(1, 1, 0),
+        Vector3f(0, 1, 1),
+        Vector3f(0, 1, 0),
+        Vector3f(0, 0, 1),
+        Vector3f(1, 0, 0),
+        Vector3f(0, 0, 0),
+
+        Vector3f(0, 0, 1),
+        Vector3f(0, 1, 0),
+        Vector3f(1, 0, 0),
+        Vector3f(0, 1, 1),
+        Vector3f(1, 0, 1),
+        Vector3f(1, 1, 0),
+        Vector3f(0, 0, 0),
+        Vector3f(1, 1, 1),
+
+        Vector3f(0, 0, 0),
+        Vector3f(1, 1, 1),
+        Vector3f(0, 1, 1),
+        Vector3f(1, 0, 1),
+        Vector3f(1, 1, 0),
+        Vector3f(1, 0, 0),
+        Vector3f(0, 1, 0),
+        Vector3f(0, 0, 1),
     };
 
     std::vector<Vector2f> tex_coords =
@@ -54,11 +107,26 @@ namespace demo
 	    Vector2f(1, 0),
 	    Vector2f(1, 1),
 	    Vector2f(0, 1),
-
+        Vector2f(0, 0),
+        Vector2f(1, 0),
         Vector2f(1, 1),
         Vector2f(0, 1),
         Vector2f(0, 0),
-        Vector2f(1, 0)
+        Vector2f(1, 0),
+        Vector2f(1, 1),
+        Vector2f(0, 1),
+        Vector2f(0, 0),
+        Vector2f(1, 0),
+        Vector2f(1, 1),
+        Vector2f(0, 1),
+        Vector2f(0, 0),
+        Vector2f(1, 0),
+        Vector2f(1, 1),
+        Vector2f(0, 1),
+        Vector2f(0, 0),
+        Vector2f(1, 0),
+        Vector2f(1, 1),
+        Vector2f(0, 1),
     };
 
     class FPSEntity : public Entity
@@ -102,8 +170,8 @@ namespace demo
 	    const int32_t handle = PengEngine::get().on_engine_initialized().subscribe([&]
 	    {
 		    const auto shader = peng::make_shared<Shader>(
-			    "resources/shaders/demo/blob_v.glsl",
-			    "resources/shaders/demo/blob_f.glsl"
+			    "resources/shaders/demo/blob.vert",
+			    "resources/shaders/demo/blob.frag"
 		    );
 
 		    const auto texture = peng::make_shared<Texture>("wall",
