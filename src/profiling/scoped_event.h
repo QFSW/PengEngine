07362056c7980aa36921ca536a3ba4c14dca4d9b
Yusuf Ismail <yusuf@qfsw.co.uk>
2023-04-13 13:02:48 +0100

    Some comments

--- src/profiling/scoped_event.h
+++ src/profiling/scoped_event.h
@@ -14,9 +14,18 @@ namespace profiling
 
         ScopedEvent(const ScopedEvent&) = delete;
         ScopedEvent(ScopedEvent&&) = delete;
+        ScopedEvent& operator=(const ScopedEvent&) = delete;
+        ScopedEvent& operator=(ScopedEvent&&) = delete;
     };
 }
 
+// Creates a scoped event marker for reporting events to external profilers
+//
+// Usage: SCOPED_EVENT(id, [extra], [color]);
+//  - Should always be placed at the start of a scope
+//  - Only 'id' is required, whereas 'extra' and 'color' are optional
+//  - The 'id' *must* be static for a given line of code - for dynamic info use 'extra'
+//  - Strings provided may safely point to temporaries since they will be immediately consumed by the profiler
 #define SCOPED_EVENT(id, ...) profiling::ScopedEvent CONCAT(__PE_event_, __LINE__)({id, __VA_ARGS__})
 
 #else
